5b97e18d597ebf3bb8b6855427b718bde3bcedb1
Charlie Delgado <charliedelgado12@icloud.com>
2023-10-04 23:05:54 -0400

    First changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Charlie Delgado 20211092</Text>
       <StatusBar style="auto" />
     </View>
   );
