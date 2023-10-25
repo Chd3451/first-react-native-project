c758c67a9208e649120541da2c2783373b42110f
Charlie Delgado <charliedelgado12@icloud.com>
2023-10-25 11:01:14 -0400

    Feat: Added the necesary archives to start again

--- App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Main from './src/componets/Main'
-import { NativeRouter } from 'react-router-native'
-
-export default function App () {
-  return <NativeRouter><Main /></NativeRouter>
-}
